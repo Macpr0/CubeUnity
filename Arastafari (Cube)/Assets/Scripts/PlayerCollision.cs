ce3a359778d0e7d032b2ab6cf2359650f8ef5c3d
Macpr0 <trexmesoud@gmail.com>
2022-11-30 23:19:45 +0100

    Changes
    1. Changed Color of Level: 1,2,3,4
    2. Added Level 5 with new colors
    3. Matched the color of the player to the background
    4. Impact sound when Player hits obstacles
    5. Main Menu and End screen have black colors instead of white

--- Arastafari (Cube)/Assets/Scripts/PlayerCollision.cs
+++ Arastafari (Cube)/Assets/Scripts/PlayerCollision.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour {
 
     public PlayerMovement movement;
+    public AudioSource bamSound;
 
+    void Start()
+    {
+        bamSound = GetComponent<AudioSource>();
+    }
     void OnCollisionEnter(Collision collisionInfo)
     {
         if (collisionInfo.collider.tag == "Obstacles")
         {
+            bamSound.Play();
             movement.enabled = false;
             FindObjectOfType<GameManager>().EndGame();
         }
